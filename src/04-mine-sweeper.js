005ec63e896ebd28e8ceb590b3e1b285fa4784a3
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-04-07 10:10:55 +0300

    4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,63 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const arr = [];
+  function mine(i, j) {
+    arr[i][j] = 10;
+    if (i === 0 && j === 0) { arr[i][j + 1] += 1; arr[i + 1][j] += 1; arr[i + 1][j + 1] += 1; }
+    if (i === 0 && j !== 0 && j !== matrix[i].length - 1) {
+      arr[i][j + 1] += 1; arr[i][j - 1] += 1;
+      arr[i + 1][j] += 1; arr[i + 1][j + 1] += 1; arr[i + 1][j - 1] += 1;
+    }
+    if (i === 0 && j === matrix[i].length - 1) {
+      arr[i][j - 1] += 1; arr[i + 1][j] += 1; arr[i + 1][j - 1] += 1;
+    }
+    if (i === matrix.length - 1 && j === 0) {
+      arr[i][j + 1] += 1; arr[i - 1][j] += 1; arr[i - 1][j + 1] += 1;
+    }
+    if (i === matrix.length - 1 && j !== 0 && j !== matrix[i].length - 1) {
+      arr[i][j + 1] += 1; arr[i][j - 1] += 1;
+      arr[i - 1][j] += 1; arr[i - 1][j + 1] += 1; arr[i - 1][j - 1] += 1;
+    }
+    if (i === matrix.length - 1 && j === matrix[i].length - 1) {
+      arr[i][j - 1] += 1; arr[i - 1][j] += 1; arr[i - 1][j - 1] += 1;
+    }
+    if (i !== 0 && i !== matrix.length - 1 && j === 0) {
+      arr[i][j + 1] += 1;
+      arr[i + 1][j] += 1; arr[i + 1][j + 1] += 1;
+      arr[i - 1][j] += 1; arr[i - 1][j + 1] += 1;
+    }
+    if (i !== 0 && i !== matrix.length - 1 && j === matrix[i].length - 1) {
+      arr[i][j - 1] += 1;
+      arr[i + 1][j] += 1; arr[i + 1][j - 1] += 1;
+      arr[i - 1][j] += 1; arr[i - 1][j - 1] += 1;
+    }
+    if (i !== 0 && i !== matrix.length - 1 && j !== 0 && j !== matrix[i].length - 1) {
+      arr[i][j + 1] += 1; arr[i][j - 1] += 1;
+      arr[i + 1][j] += 1; arr[i + 1][j + 1] += 1; arr[i + 1][j - 1] += 1;
+      arr[i - 1][j] += 1; arr[i - 1][j + 1] += 1; arr[i - 1][j - 1] += 1;
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    arr.push(matrix[i].slice());
+    for (let j = 0; j < matrix[i].length; j++) {
+      arr[i][j] = 0;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === true) mine(i, j);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] >= 10) arr[i][j] = 1;
+    }
+  }
+
+  return arr;
 }
 
 module.exports = minesweeper;
