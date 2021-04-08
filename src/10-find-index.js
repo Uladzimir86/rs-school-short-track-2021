ce3188d7118c4811c5e575aafb138f5da89bdeb1
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-04-08 23:18:03 +0300

    10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,15 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let x = 0;
+  for (let i = 0; i < 100; i++) {
+    if (array[i] === value) {
+      x = i;
+      break;
+    }
+  }
+  return x;
 }
 
 module.exports = findIndex;
