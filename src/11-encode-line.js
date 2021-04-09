b30bfa01d3575c0921a0818478d6b8ca94aa0c53
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-04-09 10:24:56 +0300

    11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,31 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const len = str.length;
+  if (len === 0) return str;
+  let i = 0;
+  let count = 1;
+  let res = '';
+  while (i < len - 1) {
+    if (str[i] === str[i + 1]) {
+      count += 1;
+      i += 1;
+    } else if (count === 1) {
+      res += `${str[i]}`;
+      i += 1;
+    } else {
+      res += `${count}${str[i]}`;
+      count = 1;
+      i += 1;
+    }
+  }
+  if (count === 1) {
+    res += `${str[i]}`;
+  } else {
+    res += `${count}${str[i]}`;
+  }
+  return res;
 }
 
 module.exports = encodeLine;
