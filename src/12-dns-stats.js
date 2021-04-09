3690e7270c63900660d961377c3367c51a4f02e9
Uladzimir86 <77360741+Uladzimir86@users.noreply.github.com>
2021-04-09 12:37:21 +0300

    12

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,35 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  let arr = [];
+  const arrTemplates = []; // массив шаблонов (доменов)
+  // ф-ция по производству шаблонов
+  function addTemplatesArr(arrr) {
+    let templ = '';
+    for (let i = 0; i < arrr.length; i++) {
+      templ += `.${arrr[i]}`;
+      arrTemplates.push(templ);
+    }
+  }
+  for (let i = 0; i < domains.length; i++) {
+    arr = domains[i].split('.').reverse();
+    addTemplatesArr(arr);
+  }
+
+  const map = new Map(); // именованная коллекция
+  for (let i = 0; i < arrTemplates.length; i++) {
+    if (map.has(arrTemplates[i])) {
+      const value = map.get(arrTemplates[i]);
+      map.set(arrTemplates[i], value + 1);
+    } else map.set(arrTemplates[i], 1);
+  }
+  // const result = {};
+  // for (const domain of map.keys()) {
+  //   result[domain] = map.get(domain);
+  // }
+
+  return Object.fromEntries(map);
 }
 
 module.exports = getDNSStats;
